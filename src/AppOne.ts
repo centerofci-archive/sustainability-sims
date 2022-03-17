069e3b1e3cfce19f7ebe1ee506227e060be33903
Alexander James Phillips <ajp.global@gmail.com>
2022-03-17 22:47:03 +0000

    Get volumetric atmosphere for planet

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,11 +1,12 @@
-import * as BABYLON from 'babylonjs'
+import * as BABYLON from "babylonjs"
+import { AtmosphericScatteringPostProcess } from "./shaders/atmosphericScattering";
 export class AppOne {
     engine: BABYLON.Engine;
     scene: BABYLON.Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
         this.engine = new BABYLON.Engine(canvas)
-        window.addEventListener('resize', () => {
+        window.addEventListener("resize", () => {
             this.engine.resize();
         });
         this.scene = createScene(this.engine, this.canvas)
@@ -35,30 +36,41 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
 
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+    // This creates and positions a camera
+    // var camera = new BABYLON.ArcRotateCamera("camera1", 0.9, 0.8, 10, BABYLON.Vector3.Zero(), scene);
+    let camera = new BABYLON.ArcRotateCamera("orbitalCamera", Math.PI / 2, Math.PI / 3, 400, BABYLON.Vector3.Zero(), scene);
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    const sun_direction = new BABYLON.Vector3(0, 0, 0)
+    var sun2 = new BABYLON.PointLight("dir01", sun_direction, scene);
+    sun2.position = new BABYLON.Vector3(500, 200, 0);
+    sun2.intensity = 0.05;
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    var shadowGenerator = new BABYLON.ShadowGenerator(1024, sun2);
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-
+    const planetRadius = 100;
+    var earth: BABYLON.Mesh = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: planetRadius * 2, segments: 32 }, scene);
+    camera.setTarget(earth);
+    shadowGenerator.addShadowCaster(earth)
+    
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+    earth.position.y = 1;
 
-    // Our built-in 'ground' shape.
+    // add atmosphere
+    const atmosphereRadius = planetRadius * 1.1;
+    let atmosphere = new AtmosphericScatteringPostProcess("atmosphere", earth, planetRadius, atmosphereRadius, sun2, camera, scene);
+
+    
+
+    // Our built-in "ground" shape.
     var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    ground.receiveShadows = true
+    
 
     return scene;
 };
---
+++ src/shaders/atmosphericScattering.ts
@@ -0,0 +1,108 @@
+import * as BABYLON from "babylonjs"
+
+
+interface AtmosphereSettings {
+    planetRadius: number, // changes the value used as the minimum height of the atmosphere
+    atmosphereRadius: number, // changes the value used as the maximum height of the atmosphere
+    falloffFactor: number, // changes the pace at whitch the density of the atmosphere decreases
+    intensity: number, // changes the intensity of the colors scattered
+    scatteringStrength: number, // changes the dispersion of the three wavelengths
+    densityModifier: number, // changes the overall density of the atmosphere
+    redWaveLength: number, // changes the value used as the red wavelength in nanometers
+    greenWaveLength: number, // same but green
+    blueWaveLength: number, // same but blue
+}
+
+export class AtmosphericScatteringPostProcess extends BABYLON.PostProcess {
+
+    settings: AtmosphereSettings;
+    camera: BABYLON.Camera;
+    sun: BABYLON.Mesh | BABYLON.PointLight;
+    planet: BABYLON.Mesh;
+
+    constructor(name: string, planet: BABYLON.Mesh, planetRadius: number, atmosphereRadius: number, sun: BABYLON.Mesh | BABYLON.PointLight, camera: BABYLON.Camera, scene: BABYLON.Scene) {
+        // you might need to change the path to the .fragment.fx file
+        super(name, "atmosphericScattering", [
+            "sunPosition",
+            "cameraPosition",
+
+            "projection",
+            "view",
+
+            "cameraNear",
+            "cameraFar",
+
+            "planetPosition",
+            "planetRadius",
+            "atmosphereRadius",
+
+            "falloffFactor",
+            "sunIntensity",
+            "scatteringStrength",
+            "densityModifier",
+
+            "redWaveLength",
+            "greenWaveLength",
+            "blueWaveLength"
+        ], [
+            "textureSampler",
+            "depthSampler",
+        ], 1, camera, BABYLON.Texture.BILINEAR_SAMPLINGMODE, scene.getEngine());
+
+        this.settings = {
+            planetRadius: planetRadius,
+            atmosphereRadius: atmosphereRadius,
+            falloffFactor: 15,
+            intensity: 15,
+            scatteringStrength: 1,
+            densityModifier: 1,
+            redWaveLength: 700,
+            greenWaveLength: 530,
+            blueWaveLength: 440,
+        };
+
+        this.camera = camera;
+        this.sun = sun;
+        this.planet = planet;
+
+        this.setCamera(this.camera);
+
+        let depthRenderer = new BABYLON.DepthRenderer(scene);
+        scene.customRenderTargets.push(depthRenderer.getDepthMap());
+        let depthMap = depthRenderer.getDepthMap();
+
+        this.onApply = (effect: BABYLON.Effect) => {
+
+            effect.setTexture("depthSampler", depthMap);
+
+            effect.setVector3("sunPosition", this.sun.getAbsolutePosition());
+            effect.setVector3("cameraPosition", this.camera.position);
+
+            effect.setVector3("planetPosition", this.planet.absolutePosition);
+
+            effect.setMatrix("projection", this.camera.getProjectionMatrix());
+            effect.setMatrix("view", this.camera.getViewMatrix());
+
+            effect.setFloat("cameraNear", camera.minZ);
+            effect.setFloat("cameraFar", camera.maxZ);
+
+            effect.setFloat("planetRadius", this.settings.planetRadius);
+            effect.setFloat("atmosphereRadius", this.settings.atmosphereRadius);
+
+            effect.setFloat("falloffFactor", this.settings.falloffFactor);
+            effect.setFloat("sunIntensity", this.settings.intensity);
+            effect.setFloat("scatteringStrength", this.settings.scatteringStrength);
+            effect.setFloat("densityModifier", this.settings.densityModifier);
+
+            effect.setFloat("redWaveLength", this.settings.redWaveLength);
+            effect.setFloat("greenWaveLength", this.settings.greenWaveLength);
+            effect.setFloat("blueWaveLength", this.settings.blueWaveLength);
+        };
+    }
+
+    setCamera(camera: BABYLON.Camera) {
+        this.camera.detachPostProcess(this);
+        this.camera = camera;
+        camera.attachPostProcess(this);
+    }
+}
