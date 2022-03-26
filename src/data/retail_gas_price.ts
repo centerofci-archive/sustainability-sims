c266d2338f26339c9628dfea7d55d04dca8d2110
Alexander James Phillips <ajp.global@gmail.com>
2022-03-26 17:56:10 +0000

    First attempt to fix gas consumption data. lol

--- src/data/retail_gas_price.ts
+++ src/data/retail_gas_price.ts
@@ -4,38 +4,56 @@ import { DATES } from "./dates"
 
 
 
-const average_uk_gas_price_2019: TemporalRangeValue = {
-    // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2019.png
-    // original page: https://tradingeconomics.com/commodity/uk-natural-gas
-    // access date: 2022-03-25
-    // Average value estimated by eye
-    value: 39.4,
-    units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
-    date_from: DATES.date_2019_01_01,
-    date_to: DATES.date_2020_01_01,
-}
+const average_uk_gas_price_2019: [TemporalRangeValue, ...Partial<TemporalRangeValue>[]] = [
+    {
+        // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2019.png
+        // original page: https://tradingeconomics.com/commodity/uk-natural-gas
+        // access date: 2022-03-25
+        // Average value estimated by eye
+        value: 39.4,
+        units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
+        date_from: DATES.date_2019_01_01,
+        date_to: DATES.date_2020_01_01,
+    },
+    {
+        value: 0.394,
+        description: "Updated to fix error in value",
+    },
+]
 
-const average_uk_gas_price_2020: TemporalRangeValue = {
-    // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2020.png
-    // original page: https://tradingeconomics.com/commodity/uk-natural-gas
-    // access date: 2022-03-25
-    // Average value estimated by eye
-    value: 24.7,
-    units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
-    date_from: DATES.date_2020_01_01,
-    date_to: DATES.date_2021_01_01,
-}
+const average_uk_gas_price_2020: [TemporalRangeValue, ...Partial<TemporalRangeValue>[]] = [
+    {
+        // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2020.png
+        // original page: https://tradingeconomics.com/commodity/uk-natural-gas
+        // access date: 2022-03-25
+        // Average value estimated by eye
+        value: 24.7,
+        units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
+        date_from: DATES.date_2020_01_01,
+        date_to: DATES.date_2021_01_01,
+    },
+    {
+        value: 0.247,
+        description: "Updated to fix error in value",
+    },
+]
 
-const average_uk_gas_price_2021: TemporalRangeValue = {
-    // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2021.png
-    // original page: https://tradingeconomics.com/commodity/uk-natural-gas
-    // access date: 2022-03-25
-    // Average value estimated by eye
-    value: 115,
-    units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
-    date_from: DATES.date_2021_01_01,
-    date_to: DATES.date_2022_01_01,
-}
+const average_uk_gas_price_2021: [TemporalRangeValue, ...Partial<TemporalRangeValue>[]] = [
+    {
+        // source: https://pod.inrupt.com/ajp/public/2022-03-25_tradingeconomics.com_uk_natural_gas__2021.png
+        // original page: https://tradingeconomics.com/commodity/uk-natural-gas
+        // access date: 2022-03-25
+        // Average value estimated by eye
+        value: 115,
+        units: { num: [UnitsID.currency_gbp], denom: [UnitsID.energy_therm] },
+        date_from: DATES.date_2021_01_01,
+        date_to: DATES.date_2022_01_01,
+    },
+    {
+        value: 1.15,
+        description: "Updated to fix error in value",
+    },
+]
 
 
 export const gas_price = {
--- src/data_support/value.ts
+++ src/data_support/value.ts
@@ -1,10 +1,20 @@
-import { Units } from "./units/units"
+import { Units } from "./units/utils"
+
+
+// export interface Source
+// {
+//     url: string
+//     access_date: Date
+// }
 
 
 export interface Value
 {
     value: number
     units: Units
+    // title?: string
+    description?: string
+    // sources?: Source[]
 }
 
 
@@ -24,3 +34,43 @@ export interface TemporalRangeValue extends Value
 // {
 
 // }
+
+
+export function get_latest_value <T extends Value> (value: T | [T, ...Partial<T>[]]): T
+{
+    if (!(value instanceof Array)) return value
+
+    return get_value(value, value.length - 1)
+}
+
+
+export function get_value <T extends Value> (value: T | [T, ...Partial<T>[]], version: number): T
+{
+    if (!(value instanceof Array)) return value
+
+    return compose_to_version_n(value, version)
+}
+
+
+
+function compose_to_version_n <T extends Value> (value: [T, ...Partial<T>[]], version: number): T
+{
+    if (version >= value.length)
+    {
+        const str = (
+            `Attempting to get composed value from version "${version}", ` +
+            `but only "${value.length}" versions available.`
+        )
+        console.error(str)
+    }
+
+    let current_version = 0
+    let composed_value: T = value[0]
+    while (current_version < version)
+    {
+        ++current_version
+        composed_value = { ...composed_value, ...value[current_version] }
+    }
+
+    return composed_value
+}
