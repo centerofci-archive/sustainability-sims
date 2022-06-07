8f0c2ca2226068c6de336911c874d800fad63518
Alexander James Phillips <ajp.global@gmail.com>
2022-06-07 21:10:32 +0100

    Revert "Failed attempt to reuse preact - page hangs"
    This reverts commit b43697a69d344927d2f8d236fe306165f895ef2b.

--- src/main2.tsx
+++ src/main2.tsx
@@ -1,14 +1,12 @@
 import { Vector3, Color3, Mesh } from "@babylonjs/core"
-import { h, render } from "preact"
-import { useRef, useState } from "preact/hooks"
-
+import React, { useRef, useState } from "react"
 
 import { useClick, useHover, useBeforeRender, Engine, Scene, BabylonNode, FiberBoxPropsCtor, FiberMeshProps } from "react-babylonjs"
-
+import { Provider } from "react-redux"
 
 import { AppTwo } from "./AppTwo"
 import { get_store } from "./scenes/create_sustainable_home_scene/state/store"
-
+import { render } from "react-dom"
 
 
 window.addEventListener("DOMContentLoaded", () => {
